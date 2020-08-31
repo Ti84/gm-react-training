86ff0e3f48158ba0b7f4950f38a7eba4b82a2d5a
Shane4666 <sc897@nau.edu>
2020-08-30 18:10:00 -0700

    React router setup.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './Normalize.css';
 import './App.css';
+import { BrowserRouter, Switch } from 'react-router-dom';
+import Recipes from './components/Recipes';
 
 function App() {
   return (
-    <div className="App">
-      Recipe buddy.
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <Recipes path="/" />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
