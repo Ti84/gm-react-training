1ee1e3ca5b2fe0ead820b3d40ac612067f90fc66
Shane4666 <sc897@nau.edu>
2020-08-30 23:54:19 -0700

    recipe card update to link.

--- src/components/RecipesList.js
+++ src/components/RecipesList.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 // Named so we can debug easier. Possible to do an anonomous function here but don't do that.
 const RecipesList = ({ recipes }) => {
   return (
     <div>
       {recipes && recipes.length ? (
-        <div className="recipe-list">{recipes.map(recipe => <div key={recipe.idMeal} className="recipe-card">
-            <img src={recipe.strMealThumb} alt={recipe.strMeal}/>
-            <h3>{recipe.strMeal}</h3>
-          </div>)}</div>
+        <div className="recipe-list">
+          {recipes.map((recipe) => (
+            <Link key={recipe.idMeal} to={`/${recipe.idMeal}`}>
+              <div className="recipe-card">
+                <img src={recipe.strMealThumb} alt={recipe.strMeal} />
+                <h3>{recipe.strMeal}</h3>
+              </div>
+            </Link>
+          ))}
+        </div>
       ) : (
         <div>No Recipes found!</div>
       )}
