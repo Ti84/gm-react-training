24a206adaaabfd6f6c09657b6606450d3683d113
Shane4666 <sc897@nau.edu>
2020-09-13 19:27:37 -0700

    style updates.

--- src/components/RecipeDetails.js
+++ src/components/RecipeDetails.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const RecipeDetails = () => {
-  // https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772
   const { recipeId } = useParams();
   const [recipeDetails, setRecipeDetails] = useState(null);
   const [ingredients, setIngredients] = useState([]);
@@ -36,7 +35,6 @@ const RecipeDetails = () => {
 
         setRecipeDetails(mealData);
         setIngredients(filteredIngredients);
-        console.log(mealData);
       } catch (e) {
         console.error(e);
       }
@@ -52,33 +50,41 @@ const RecipeDetails = () => {
           <h2>
             {recipeDetails.strMeal} - {recipeDetails.strArea}
           </h2>
-          <h3>Ingredients:</h3>
-          <ul className="recipe-details-list">
-            {ingredients &&
-              ingredients.map((ingredient) => <li key={ingredient}>{ingredient}</li>)}
-          </ul>
-          <h3>Instructions: </h3>
-          <p>{recipeDetails.strInstructions}</p>
-          <div>
-            <span>Recipe Source: </span>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href={recipeDetails.strSource}
-            >
-              {recipeDetails.strSource}
-            </a>
-          </div>
-          <div>
-            <span>Youtube Source: </span>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href={recipeDetails.strYoutube}
-            >
-              {recipeDetails.strYoutube}
-            </a>
-          </div>
+          <section>
+            <h3>Ingredients:</h3>
+            <ul className="recipe-details-list">
+              {ingredients &&
+                ingredients.map((ingredient) => (
+                  <li key={ingredient}>{ingredient}</li>
+                ))}
+            </ul>
+          </section>
+          <section>
+            <h3>Instructions: </h3>
+            <p>{recipeDetails.strInstructions}</p>
+          </section>
+          <section className="resources">
+            <div>
+              <span>Recipe Source: </span>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href={recipeDetails.strSource}
+              >
+                {recipeDetails.strSource}
+              </a>
+            </div>
+            <div>
+              <span>Youtube Source: </span>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href={recipeDetails.strYoutube}
+              >
+                {recipeDetails.strYoutube}
+              </a>
+            </div>
+          </section>
         </div>
       ) : (
         <h2>Loading...</h2>
